c0576cac160023061a29f2c38146c5095b9c5b5b
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2023-03-18 15:20:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ SmoothLibrary.createDialog = function(title, text, status){
   let promise = new Promise((resolve, reject) => {
     let div = document.createElement('div');
     div.innerHTML = (`
-    <font size="20px" color="rgb(255,244,244)"><center><span>${title}</span><center></font><br>
-    <font size="4px" color="rgb(255,255,255)"><span>${text}</span>
+    <font size="20px" color="rgb(179, 179, 179)"><center><span>${title}</span><center></font><br>
+    <font size="4px" color="rgb(222, 222, 222)"><span>${text}</span>
     `);
     
-    div.style.backgroundColor = "rgb(60, 60, 60)";
+    div.style.backgroundColor = "rgb(41, 41, 41)";
     div.style.width  = "30%";
     div.style.height = "20%";
+    div.style.position = "absolute";
     div.style.top = "50%";
     div.style.left = "50%";
     document.body.appendChild(div);
