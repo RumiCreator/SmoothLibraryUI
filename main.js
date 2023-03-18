3b3980a3aca1594012184ee4821fb0c39765e62a
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2023-03-18 15:31:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,13 +12,20 @@ SmoothLibrary.createDialog = function(title, text, status){
     <font size="4px" color="#cfcfcf"><span>${text}</span>
     `);
     
+    centerSquare = (() => {
+      div.style.marginLeft = window.innerWidth/2 - div.clientWidth/2 + "px";
+      div.style.marginTop = window.innerHeight/2 - div.clientHeight/2 + "px";
+    });
+    
+    centerSquare();
+    
     div.style.backgroundColor = "#303030";
     div.style.width  = "30%";
     div.style.height = "20%";
     div.style.position = "absolute";
-    div.style.marginLeft = window.innerWidth/2 - div.clientWidth/2 + "px";
-    div.style.marginTop = window.innerHeight/2 - div.clientHeight/2 + "px";
     document.body.appendChild(div);
+    
+    window.addEventListener('resize', centerSquare)
     
     resolve(div);
   });
