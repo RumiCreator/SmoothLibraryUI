67d35ccd7cccd454e6782d9659996aa56b1f902a
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2023-03-18 15:15:46 -0700

    Create

---
+++ main.js
@@ -0,0 +1,26 @@
+// Smooth Library
+
+const SmoothLibrary = function(){};
+
+// Function definitions,
+
+SmoothLibrary.createDialog = function(title, text, status){
+  let promise = new Promise((resolve, reject) => {
+    let div = document.createElement('div');
+    div.innerHTML = (`
+    <font size="20px" color="rgb(255,244,244)"><center><span>${title}</span><center></font><br>
+    <font size="4px" color="rgb(255,255,255)"><span>${text}</span>
+    `);
+    
+    div.style.backgroundColor = "rgb(60, 60, 60)";
+    div.style.width  = "100px";
+    div.style.height = "50px";
+    div.style.top = "50%";
+    div.style.left = "50%";
+    document.body.appendChild(div);
+    
+    resolve(div);
+  });
+  
+  return promise;
+}
